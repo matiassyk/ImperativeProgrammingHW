61f6db905f57e32a59816529254f4916fd4ba76e
Matias Mettala <matiasmettala@Matiass-MacBook-Air.local>
2021-09-01 10:25:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+//import java.util.Scanner; // #1
+//import java.lang.Math; // #2 & #3
+public class Main {
+
+    public static void main(String[] args) {
+    	// #1
+//    	Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+//		System.out.println("Enter weight");
+//		String inputW = myObj.nextLine();
+//
+//		int p = 0;
+//		int w = Integer.parseInt(inputW);
+//
+//		if (w <= 2)
+//	    	p = 3;
+//		if ((2 < w) & (w <= 5))
+//	    	p = 3 + (w - 2) * 2;
+//		if (w > 5)
+//	    	p = 3 + (2 * 3) + (w - 5) * 3;
+//        	System.out.println(p);
+
+        // #2
+//		int a = 4;
+//		double b= Math.pow(3, a);
+//		System.out.println(b);
+
+		// #3
+//		int a = 4;
+//		int b = 3;
+//		double c = Math.pow (b, a);
+//		System.out.println(c);
+
+		// #4
+		
+
+
+
+    }
+}
